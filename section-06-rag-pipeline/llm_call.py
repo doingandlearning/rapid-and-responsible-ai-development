3784e51a6f71b0a3d37fa05943cfa99fdeef0449
Kevin Cunningham <kevin@kevincunningham.co.uk>
2025-09-18 14:33:05 +0100

    adds more code

--- section-06-rag-pipeline/llm_call.py
+++ section-06-rag-pipeline/llm_call.py
@@ -1,6 +1,7 @@
 import requests
 import psycopg
 import json
+import sys
 
 DB_CONFIG = {
   "dbname": "pgvector",
@@ -9,6 +10,8 @@ DB_CONFIG = {
   "host": "localhost",
   "port": "5050"
 }
+
+request_count = 0
 
 OLLAMA_URL = "http://localhost:11434/api/embed"
 MODEL = "gpt-3.5-turbo"
@@ -36,7 +39,13 @@ def get_k_nearest_neighbors(user_embedding, k=3):
     with conn.cursor() as cur:
       cur.execute(
         """
-        SELECT name, 1 - (embedding <=> %s) AS similarity
+        SELECT 
+          text,
+          id,
+          document_title,
+          page_number,
+          section_title
+          , 1 - (embedding <=> %s) AS similarity
         FROM document_chunks
         ORDER BY similarity DESC
         LIMIT %s
@@ -45,7 +54,18 @@ def get_k_nearest_neighbors(user_embedding, k=3):
 
       )
       results = cur.fetchall()
-      return results
+      search_results = []
+
+      for text, id, document_title, page_number, section_title, similarity in results:
+        if similarity >= 0.4:
+          search_results.append(
+            f"""
+            Text: {text} 
+            Source: {document_title} on page {page_number}  [section: {section_title}]
+            Similarity score: {similarity}
+            """
+          )
+      return search_results
 
 def query_llm(user_query, relevant_documents):
 
@@ -53,7 +73,8 @@ def query_llm(user_query, relevant_documents):
   You are an AI assistant for Edinburgh University's IT Services.
   Your role and responsibilities:
   - Provide accurate, helpful answers using ONLY the context from official Edinburgh University documents
-- Always cite your sources using the format: (Source: Document Name, Page X)
+- Always cite your sources using the format: (Source: Document Name, Page X, Section (if exists))
+- There will be up to three chunks of relevant documents - reference all that you use for your response in the required format
 - If the context doesn't contain relevant information, clearly state "I don't have that information in the available documents"
 - Use professional, helpful language appropriate for university staff and students
 - Focus on practical, actionable guidance
@@ -64,6 +85,51 @@ def query_llm(user_query, relevant_documents):
   Context from Edinburgh University documents:
 
   {relevant_documents}
+
+  User question: {user_query}
+
+  Please provide a helpful, accurate answer based on the context above. Remember to cite your sources.
+  """
+  try:
+    response = requests.post(LLM_ENDPOINT,
+    headers={
+      "Authorization": f"Bearer {API_KEY}",
+      "Content-Type": "application/json"
+    },
+    json={
+      "model": MODEL,
+      "messages": [
+        {"role": "system", "content":  system_prompt},
+        {"role": "user", "content": user_prompt}
+      ],
+      "temperature": 0.2,
+      "max_tokens": 600 
+    })
+
+    data = response.json()
+    return {"answer": data["choices"][0]["message"]["content"]}
+  except Exception as e:
+    return {
+      'answer': "Something went wrong"
+    }
+def verify_llm_response(llm_response, user_query):
+
+  system_prompt = """
+  You are a university auditor - return a score from 0-5 based on the following criteria.
+  Only return that score - nothing else - no supporting text - that's it - i mean it!!
+
+  - Professional tone
+  - Actionable
+  - Sources clearly attributed
+  - Actually answers  the user query
+  - Up to date information
+  """
+
+
+  user_prompt = f"""
+  The LLM response:
+
+  {llm_response}
 
   User question: {user_query}
 
@@ -102,5 +168,19 @@ user_embedding = get_embeddings(user_query)
 relevant_documents = get_k_nearest_neighbors(user_embedding)
 
 # Call to our LLM
+if len(relevant_documents) == 0:
+  print("I don't know how to help. Have you tried turning it on and off again?")
+  sys.exit(1)
+
+print(relevant_documents)
 llm_response = query_llm(user_query, relevant_documents)
+
+# Check it does have source
+# Check that it 
+score_response = verify_llm_response(llm_response, user_query)
+
+with open("log.txt", "a") as file:
+  file.write(f"{score_response} - other metadata blah")
+
+
 print(llm_response["answer"])
